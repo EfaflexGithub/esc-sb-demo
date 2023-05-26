1a84297287428d21b535b155369436cc1abd722a
Fischerauer Christian <christian.fischerauer@efaflex.com>
2023-05-26 09:01:46 +0200

    feat: :tada: setup flutter project
    used template: empty

---
+++ README.md
@@ -0,0 +1,3 @@
+# efa_smartconnect_modbus_demo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/efa_smartconnect_modbus_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.efa_smartconnect_modbus_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
